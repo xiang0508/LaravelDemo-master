b75339118e17613197a1207321b8d562b82911ee
xiang0508 <H24663088@mailst.cjcu.edu.tw>
2020-12-01 13:38:05 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InspiringController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello-world', function () {
+
+    return view('hello_world');
+
+}); 
+
+Route::get('/about_us', function () {
+
+    return view('about_us',['name' => 'Laravel 範例']);
+
+});
+
+Route::get('/inspire', 'App\Http\Controllers\InspiringController@inspire');
